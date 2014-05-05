a75436750c0118701dc00a7f06a50bf04f7e1397
Ankur Zilpelwar <ankur.zilpelwar@gmail.com>
2014-05-05 15:14:02 +0400

    Add final exam question 7 solution program

---
+++ src/main/java/com/ankurMongoHWProb/Final_Q7.java
@@ -0,0 +1,28 @@
+package com.ankurMongoHWProb;
+
+import com.mongodb.*;
+
+import java.io.IOException;
+
+
+public class Final_Q7 {
+    public static void main(String[] args) throws IOException {
+        MongoClient c =  new MongoClient(new MongoClientURI("mongodb://localhost"));
+        DB db = c.getDB("finaltask");
+        int i =0;
+        DBCollection album = db.getCollection("albums");
+        DBCollection image = db.getCollection("images");
+
+        DBCursor cur = image.find();
+        cur.next();
+
+        while (cur.hasNext()){
+            Object id = cur.curr().get("_id");
+            DBCursor curalbum = album.find(new BasicDBObject("images", id));
+            if(!curalbum.hasNext()){
+                image.remove(new BasicDBObject("_id", id));
+            }
+            cur.next();
+        }
+    }
+}
